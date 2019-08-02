a28fdc38afc1a8398a0924d47a1f7231ea1c1c66
Daniel Litvak <daniel@daniel.litvak>
2019-08-03 01:16:45 +0300

    Add form validation server side

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+function validateRequest(req) {
+  const USERNAME = req.body.username;
+  const PASSWORD = req.body.password;
+
+  if (req && USERNAME && PASSWORD) {
+    if (PASSWORD.length < 2)
+      return 'something failed';
+    else {
+      return 'success';
+    }
+  }
+  return 'something failed'
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.post('/*',(req, res, next) => {
+
+  res.render('index',{result: validateRequest(req)});
+})
 
 module.exports = router;
