72fb2a64448277b74439488fcf4bbfd85c6227c4
Daniel Litvak <daniel@daniel.litvak>
2019-08-04 23:20:57 +0300

    Fix eslint errors

--- public/javascripts/source.js
+++ public/javascripts/source.js
@@ -1,15 +1,20 @@
+/* eslint-disable no-unused-vars */
 const FORM_MESSAGE = document.getElementById('form-message');
 const FORM = document.getElementById('login-form');
 const USERNAME = document.getElementById('username');
 const PASSWORD = document.getElementById('password');
 
-function checkForm() {  
+/**
+ * Checks the login form.
+ * @return {boolean} true if login data is valid, otherwise false.
+ */
+function checkForm() {
   if (!USERNAME.value || !PASSWORD.value) {
     FORM_MESSAGE.innerText = 'all fileds must be filled';
-    FORM_MESSAGE.style.color = 'red'
+    FORM_MESSAGE.style.color = 'red';
     setTimeout(() => {
       FORM_MESSAGE.innerText = '';
-    },3000)
+    }, 3000);
     return false;
   }
   return true;
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = new express.Router();
 
+/**
+ * Validates the post request return an answer
+ * @param {Request} req post request
+ * @return {String} an answer for the validation
+ */
 function validateRequest(req) {
   const USERNAME = req.body.username;
   const PASSWORD = req.body.password;
 
   if (req && USERNAME && PASSWORD) {
-    if (PASSWORD.length < 2)
+    if (PASSWORD.length < 2) {
       return 'something failed';
-    else {
+    } else {
       return 'success';
     }
   }
-  return 'something failed'
+  return 'something failed';
 }
 
 /* GET home page. */
@@ -20,9 +25,8 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.post('/*',(req, res, next) => {
-
-  res.render('index',{result: validateRequest(req)});
-})
+router.post('/*', (req, res, next) => {
+  res.render('index', {result: validateRequest(req)});
+});
 
 module.exports = router;
