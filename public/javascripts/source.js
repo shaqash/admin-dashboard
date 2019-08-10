8023d5e602362b35aa1455bd1ef1080c4143b13a
Daniel Litvak <daniel@daniel.litvak>
2019-08-10 15:45:32 +0300

    Add edit and trash buttons for users in the users table

--- public/javascripts/source.js
+++ public/javascripts/source.js
@@ -1,3 +1,57 @@
+/**
+ * Creates a html elemtent.
+ *
+ * @param {string} tag the element tag
+ * @param {string} text the inner text
+ * @return {object} html element
+ */
+function makeElement(tag, text) {
+  const element = document.createElement(tag);
+  element.innerText = text;
+  return element;
+}
+/**
+ * Creates button with icon.
+ * @param {string} icon font awesome icon text
+ * @return {object} button element
+ */
+function createButton(icon) {
+  const button = document.createElement('button');
+  button.classList.add('btn', 'btn-default');
+  const iconElement = document.createElement('i');
+  iconElement.classList.add('fas', icon, 'icon-user');
+  button.appendChild(iconElement);
+  return button;
+}
+
+/**
+ * Creates a row element.
+ *
+ * @param {string} index
+ * @param {string} username
+ * @param {string} password
+ * @param {string} name
+ * @return {object} row element
+ */
+function createRow(index, username, password, name) {
+  const row = document.createElement('tr');
+  const indexElement = makeElement('td', index);
+  row.appendChild(indexElement);
+  const usernameElement = makeElement('td', username);
+  row.appendChild(usernameElement);
+  const passwordElement = makeElement('td', password);
+  row.appendChild(passwordElement);
+  const nameElement = makeElement('td', name);
+  row.appendChild(nameElement);
+  const editButton = document.createElement('td');
+  editButton.appendChild(createButton('fa-edit'));
+  row.appendChild(editButton);
+  const trashButton = document.createElement('td');
+  trashButton.appendChild(createButton('fa-trash'));
+  row.appendChild(trashButton);
+  return row;
+}
+
 // Toggle side bar
 const toggleButton = document.getElementById('menu-toggle');
 const wrapper = document.getElementById('wrapper');
@@ -14,19 +68,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
         return res.json();
       }).then((json) => {
         json.forEach((value, key) => {
-          const row = document.createElement('tr');
-          const index = document.createElement('td');
-          index.innerText=key + 1;
-          row.appendChild(index);
-          const username = document.createElement('td');
-          username.innerText = value.username;
-          row.appendChild(username);
-          const password = document.createElement('td');
-          password.innerText = value.password;
-          row.appendChild(password);
-          const name = document.createElement('td');
-          name.innerText = value.name;
-          row.appendChild(name);
+          const row = createRow(key+1, value.username, value.password,
+              value.name);
           tableBody.appendChild(row);
         });
         usersTable.style.display='table';
