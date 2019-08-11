9c5d6b2046ef8b92f32f2ff8a26a3a8f6a681d51
Shaked Ashkenazi <shaked.a21@gmail.com>
2019-08-11 21:26:23 +0300

    Implement user router

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = new express.Router();
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const usersRouter = require('./users');
 
 /**
  * Validates the post request return an answer
@@ -30,56 +31,11 @@ router.get('/home', (req, res, next) => {
   }
 });
 
+router.use('/users/', usersRouter);
+
 router.get('/sign-out', (req, res, next) => {
   req.session.destroy();
   res.redirect('/login');
-});
-
-router.get('/users.json', (req, res, next)=>{
-  if (!req.session.username) {
-    res.status(403).send('403 - forbidden');
-  } else {
-    User.find().then((users) => {
-      res.send(users);
-    });
-  }
-});
-
-router.post('/sign-up', (req, res, next) => {
-  const {name, username, password, password2} = req.body;
-  if (!name || !username || !password || !password2) {
-    res.render('sign-up', {error: 'not all fields are filled'});
-  } else if (password !== password2) {
-    res.render('sign-up', {error: 'passwords must match'});
-  } else if (password.length < 6) {
-    res.render('sign-up', {error: 'password needs to be 6 characters or ' +
-     'longer'});
-  } else {
-    User.findOne({username: username}).then((user) => {
-      if (user) {
-        res.render('sign-up', {error: 'username already exits'});
-      } else {
-        bcrypt.hash(password, 10, (err, hash) => {
-          const user = new User({name, username, password: hash});
-          user.save().then(() => {
-            res.render('login', {success: 'Sign up successful, please log in'});
-          });
-        });
-      }
-    });
-  }
-});
-
-router.delete('/users/:username/', (req, res, next) => {
-  const {username} = req.params;
-  if (!req.session.username) {
-    res.status(403).send('403 - forbidden');
-  } else {
-    User.deleteOne({'username': username}, (err) => {
-      if (err) return next(err);
-      res.render('home', {success: 'Deletion successful'});
-    });
-  }
 });
 
 router.post('/login', (req, res, next) => {
